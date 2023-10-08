35371f6d4fc5d75cb529b0951cee03e7799a685f
Jill Henderson <jillian.bethany.henderson@gmail.com>
2023-10-08 14:57:03 +0100

    ingredients poplulated with foodgroups, get restest for food-group working

--- foodgroups/urls.py
+++ foodgroups/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FoodGroupListView
 
 urlpatterns = [
-    path('<int:pk>/', FoodGroupListView.as_view()),
+    path('', FoodGroupListView.as_view()),
 ]
